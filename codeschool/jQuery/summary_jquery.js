d679ab6955185b699db0172b16367377bcc6786d
Guillaume Diaz <guihome-diaz@users.noreply.github.com>
2014-12-25 14:38:27 +0100

    Add JQuery

---
+++ codeschool/jQuery/summary_jquery.js
@@ -0,0 +1,21 @@
+// =============================================================================
+//                                JQuery  summary
+//==============================================================================
+
+
+// ======== Wait for HTML DOM to be loaded ========
+jQuery(document).ready(function(){
+    <code>
+}); 
+
+
+
+// ======== Selector ========
+
+// Get full element
+jQuery("h1");   ==    $("h1");
+                      $("p span");
+                      
+// Get element content 
+$("h1").text();
+
