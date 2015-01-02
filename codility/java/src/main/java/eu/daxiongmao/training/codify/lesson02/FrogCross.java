a2dbde6ddbadd1250f24891a746cdbc7fb62a27b
Guillaume Diaz <guihome-diaz@users.noreply.github.com>
2015-01-02 22:12:59 +0100

    Codility lesson 02 - test 02 - Frog river crossing

---
+++ codility/java/src/main/java/eu/daxiongmao/training/codify/lesson02/FrogCross.java
@@ -0,0 +1,119 @@
+package eu.daxiongmao.training.codify.lesson02;
+
+
+public class FrogCross {
+
+  /**
+   * <p>
+   * To compute how much time it will take to the frog to cross the river. <br/>
+   * Result time depends on the available leaves and their time.<br/>
+   * Each minute a new leaf is available. <br/>
+   * Leaves and time are represented in array[minute]=leaf_index
+   * </p>
+   * <p>
+   * This version deals with HUMAN logic. That means it <strong>based on 1...X</strong> <br/>
+   * From a computer perspective this will create an array int[riverSize + 1] with index[O] not
+   * used.
+   * </p>
+   *
+   * @param riverSize total river size
+   * @param leavesTime Each minute a new leaf is available. <br/>
+   *        Leaves and time are represented in array[minute]=leaf_index
+   * @return -1 if the frog cannot cross the river || else the required time in minute
+   */
+  int crossRiver_humanLogic(final int riverSize, final int[] leavesTime) {
+    // Check args
+    if (leavesTime == null || leavesTime.length == 0) {
+      return -1;
+    }
+    if (riverSize == 0) {
+      return 0;
+    }
+
+    // Default result is 'not possible'
+    int result = -1;
+    int currentPosition = 0;
+
+    // Array starting at 0 (but not used). Real values from 1...riverSize
+    final int currentLeaves[] = new int[riverSize + 1];
+
+    for (int i = 0; i < leavesTime.length; i++) {
+      // Register the current leave as fallen
+      currentLeaves[leavesTime[i]] = 1;
+
+      if (leavesTime[i] == currentPosition + 1) {
+        // Frog can move!
+        currentPosition++;
+        // Check for other available moves as well (!! Human logic array must be adjusted !!)
+        while (currentPosition < riverSize && currentLeaves[currentPosition + 1] == 1) {
+          currentPosition++;
+        }
+        // Is the river crossed ?
+        if (currentPosition == riverSize) {
+          result = i;
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+
+  /**
+   * <p>
+   * To compute how much time it will take to the frog to cross the river. <br/>
+   * Result time depends on the available leaves and their time.<br/>
+   * Each minute a new leaf is available. <br/>
+   * Leaves and time are represented in array[minute]=leaf_index
+   * </p>
+   * <p>
+   * This version deals with COMPUTER logic. That means it <strong>based on 0...X-1</strong><br/>
+   * Performance wised that is a bit better
+   * </p>
+   *
+   * @param riverSize total river size
+   * @param leavesTime Each minute a new leaf is available. <br/>
+   *        Leaves and time are represented in array[minute]=leaf_index
+   * @return -1 if the frog cannot cross the river || else the required time in minute
+   */
+  public int crossRiver(final int riverSize, final int[] leavesTime) {
+    // Check args
+    if (leavesTime == null || leavesTime.length == 0) {
+      return -1;
+    }
+    if (riverSize == 0) {
+      return 0;
+    }
+
+    // Default result is 'not possible'
+    int result = -1;
+    int currentPosition = 0;
+
+    // Array starting at 0...(riverSize - 1)
+    final int currentLeaves[] = new int[riverSize];
+
+    for (int i = 0; i < leavesTime.length; i++) {
+      // Register the current leave as fallen (!! Computer logic you should think of array
+      // restriction !!)
+      currentLeaves[leavesTime[i] - 1] = 1;
+
+      if (leavesTime[i] == currentPosition + 1) {
+        // Frog can move!
+        currentPosition++;
+        // Check for other available moves as well
+        while (currentPosition < riverSize && currentLeaves[currentPosition] == 1) {
+          currentPosition++;
+        }
+        // Is the river crossed ?
+        if (currentPosition == riverSize) {
+          result = i;
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+}
---
+++ codility/java/src/test/java/eu/daxiongmao/training/codify/lesson02/FrogCrossTest.java
@@ -0,0 +1,120 @@
+package eu.daxiongmao.training.codify.lesson02;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FrogCrossTest {
+
+  @Test
+  public void testNoCross() {
+    // Dataset
+    final int testArray[] = new int[] {1, 2, 3, 4, 3, 5, 4};
+    final int riverSize = 0;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(0, timeRequired);
+  }
+
+  @Test
+  public void testCrossSuccessSimple() {
+    // Dataset
+    final int testArray[] = new int[] {1, 2, 3, 4, 3, 5, 4};
+    final int riverSize = 5;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(5, timeRequired);
+  }
+
+  @Test
+  public void testCrossSuccessExample() {
+    // Dataset
+    final int testArray[] = new int[] {1, 3, 1, 4, 2, 3, 5, 4};
+    final int riverSize = 5;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(6, timeRequired);
+  }
+
+
+  @Test
+  public void testCrossSuccessAlternate1() {
+    // Dataset
+    final int testArray[] = new int[] {1, 2, 4, 2, 1, 5, 3, 4, 3};
+    final int riverSize = 5;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(6, timeRequired);
+  }
+
+  @Test
+  public void testCrossSuccessAlternate2() {
+    // Dataset
+    final int testArray[] = new int[] {1, 5, 4, 3, 2, 1, 3, 7, 6};
+    final int riverSize = 7;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(8, timeRequired);
+  }
+
+  @Test
+  public void testCrossFailure1() {
+    // Dataset
+    final int testArray[] = new int[] {2};
+    final int riverSize = 2;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(-1, timeRequired);
+  }
+
+  @Test
+  public void testCrossFailure2() {
+    // Dataset
+    final int testArray[] = new int[] {2, 3, 4, 5, 6, 7, 3, 4, 5};
+    final int riverSize = 7;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(-1, timeRequired);
+  }
+
+  @Test
+  public void testCrossFailure3() {
+    // Dataset
+    final int testArray[] = new int[] {1, 2, 4, 5, 6, 1, 1, 4, 5};
+    final int riverSize = 6;
+
+    // Algo to test
+    final FrogCross algo = new FrogCross();
+    final int timeRequired = algo.crossRiver(riverSize, testArray);
+
+    // Assertions
+    Assert.assertEquals(-1, timeRequired);
+  }
+}
