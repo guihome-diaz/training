e341b03dd58511d05539963c5a9212ccf0cd4288
guihome-diaz <guillaume@qin-diaz.com>
2015-11-23 18:50:43 +0100

    Programming in Scala - chp8

--- progInScala/src/main/scala/eu/daxiongmao/training/scala/chp8/FunctionsAndClosures.scala
+++ progInScala/src/main/scala/eu/daxiongmao/training/scala/chp8/FunctionsAndClosures.scala
@@ -19,16 +19,72 @@ object FunctionsAndClosures {
 
 
     // =========== FUNCTIONS literals as variables (first-class functions) =============
+    println("Test 3 - literals as variables")
     val increase = (x: Int) => x + 1
     val y = increase(10)
     assert(y == 11)
     println("183 will be increase to: " + increase(183))
 
 
-    val increaseAlot = (x: Int) => {
+    val increaseALot = (x: Int) => {
       (x * 10 + x + 1) * x
     }
-    println("183 will be increase a lot to: " + increaseAlot(183))
+    println("183 will be increase a lot to: " + increaseALot(183))
+
+
+    // =========== Example of filter with type detection ===========
+    println("Test 4 - filtering and type detection")
+    val someNumbers = List(-11, -10, -5, 0, 5, 10)
+    // 'classic' expression
+    println("Numbers filters: " + someNumbers.filter((x: Int) => x > 0))
+    // Shortcut expression (auto detection of types)
+    println("Numbers filters (type detection): " + someNumbers.filter((x) => x > 0))
+    println("Numbers filters (type detection, short): " + someNumbers.filter(x => x > 0))
+    // Extremely short expression using '_'.
+    //   underscores as placeholders for one or more parameters,
+    //   as long as each parameter appears only one time within the function literal.
+    println("Numbers filters (type detection, '_' operator): " + someNumbers.filter(_ > 0))
+
+
+    // =========== '_' examples ===========
+    println("Test 5 - literals as variables")
+    // Usage of '_' in loops
+    println("List of numbers: ")
+    someNumbers.foreach(println _)
+    // When a function as only one arg. you don't need to put the _
+    println("List of numbers [short]: ")
+    someNumbers.foreach(println)
+    // Function alias: usage of '_' to match all arguments
+    val sumClassical = sum(1, 4, 5)
+    val sumSymlink = sum _
+    val sumModern = sumSymlink(1, 4, 5)
+    assert(sumClassical == sumModern)
+    // functions renaming
+    val sumAlternate = sum(10, _:Int, 5)
+    val sumAlternateResult = sumAlternate(7)
+    assert(22 == sumAlternateResult)
+
+
+    // =========== Closure ===========
+    println("Test 6 - Closure")
+    val constant = 90
+    val addConstant = (x: Int) => x + constant
+    println("simple closure example: " + constant + " + 12 = " + addConstant(12))
+
+
+    // =========== varargs (* operator) ===========
+    println("Test 7 - Varargs [repeated parameters]")
+    // standard use
+    echo("This", " is", " like", " Java:", " public void myMethod(String...) { }")
+    // Array use (dynamic cast with '_')
+    println("")
+    val testString = Array("This"," is", " another", " example"," using", " Array[]")
+    echo(testString: _*)
+
+
+    // ============ Functions defaults ============
+    // You can check listing 8.3 to set a default method parameter value so it can become optional
+
 
   }
 
@@ -58,4 +114,10 @@ object FunctionsAndClosures {
       printLinesWithMinWidth(line)
     }
   }
+
+  def sum(a: Int, b: Int, c: Int) = a + b + c
+
+
+  // Like Java varargs (String...)
+  def echo(args: String*) = for (arg <- args) print(arg)
 }
--- progInScala/src/main/scala/eu/daxiongmao/training/scala/chp8/Readme.txt
+++ progInScala/src/main/scala/eu/daxiongmao/training/scala/chp8/Readme.txt
@@ -2,9 +2,11 @@
 README - Scala discovery
 ////////////////////////////
 
-This chapter demonstrates functions.
+This chapter demonstrates functions and basic closures.
 
 Key points:
-You can function(s) in a function. Meaning the inner function cannot be accessible by anything else than the caller!
+- You can put function(s) in a function.
+  Meaning the inner function cannot be accessible by anything else than the caller!
+- Example of '_' and '*' operators
 
 Enjoy!
