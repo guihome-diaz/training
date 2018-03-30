845a1402d71f9f0b5ebe9b87eee5dec21bb56f63
Guillaume Diaz <guillaume@qin-diaz.com>
2018-03-30 15:24:56 +0200

    init new spring boot 2 'recipe' project

---
+++ spring5/recipe/src/main/java/eu/daxiongmao/springtraining/recipe/WebAppRecipesApplication.java
@@ -0,0 +1,12 @@
+package eu.daxiongmao.springtraining.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppRecipesApplication.class, args);
+    }
+}
