3a3ed78dbfc7af31c1cb6eafc3a435eac0ee2c66
Guillaume Diaz <guillaume@qin-diaz.com>
2021-01-27 21:08:09 +0100

    Ex8: add new components

---
+++ angular/training-app/src/app/activable-when-logged-in.spec.ts
@@ -0,0 +1,7 @@
+import { ActionableWhenLoggedIn } from './actionable-when-logged-in.service';
+
+describe('ActivableWhenLoggedIn', () => {
+  it('should create an instance', () => {
+    expect(new ActionableWhenLoggedIn()).toBeTruthy();
+  });
+});
