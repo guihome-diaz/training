f336973f3ffbe7273e05dc3f0e41441652a2e12a
Guillaume Diaz <guillaume@qin-diaz.com>
2021-06-22 20:22:37 +0200

    New java project for Codility lessons (Spring Boot 2)

---
+++ codility/codility/src/main/java/eu/daxiongmao/codility/DaxiongmaoCodilityApplication.java
@@ -0,0 +1,13 @@
+package eu.daxiongmao.codility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaxiongmaoCodilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaxiongmaoCodilityApplication.class, args);
+	}
+
+}
