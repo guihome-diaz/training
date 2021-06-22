1871ef48d96ae3d107d4fb56c69283618277c804
Guillaume Diaz <guillaume@qin-diaz.com>
2021-06-22 22:36:04 +0200

    First lesson, alternate way to compute base2

--- codility/codility/src/test/java/eu/daxiongmao/codility/lesson1/BinaryGapTest.java
+++ codility/codility/src/test/java/eu/daxiongmao/codility/lesson1/BinaryGapTest.java
@@ -11,19 +11,28 @@ public class BinaryGapTest {
     @Test
     @DisplayName("Convert number to base 2")
     public void convertToBase2() {
-        // For all integers: compare with default java algo
+
+        // Brute force for all integers: compare with default java algo
+        /*
         for (int i = 0 ; i < Integer.MAX_VALUE; i++) {
             Assertions.assertEquals(Integer.toBinaryString(i), binaryGap.convertNumberToBase2(i));
         }
+        */
 
         // Given values in examples
         Assertions.assertEquals("1001", binaryGap.convertNumberToBase2(9));
         Assertions.assertEquals("1000010001", binaryGap.convertNumberToBase2(529));
         Assertions.assertEquals("10100", binaryGap.convertNumberToBase2(20));
-        Assertions.assertEquals("111", binaryGap.convertNumberToBase2(15));
+        Assertions.assertEquals("1111", binaryGap.convertNumberToBase2(15));
         Assertions.assertEquals("100000", binaryGap.convertNumberToBase2(32));
         Assertions.assertEquals("10000010001", binaryGap.convertNumberToBase2(1041));
 
+        // Limits
+        Assertions.assertEquals(Integer.toBinaryString(0), binaryGap.convertNumberToBase2(0));
+        Assertions.assertEquals(Integer.toBinaryString(Integer.MAX_VALUE), binaryGap.convertNumberToBase2(Integer.MAX_VALUE));
+
+        // Random values
+        Assertions.assertEquals(Integer.toBinaryString(8595879), binaryGap.convertNumberToBase2(8595879));
     }
 
 }
