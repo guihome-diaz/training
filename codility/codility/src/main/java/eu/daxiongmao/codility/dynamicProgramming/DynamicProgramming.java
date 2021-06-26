12a6e12bb5d5fd7c9b6025b63cd4a415c2f5e3cf
Guillaume Diaz <guillaume@qin-diaz.com>
2021-06-26 20:50:00 +0200

    Hard challenge, ongoing work. 3 nights and counting. This time 81% of success [issue with correctness]

--- codility/codility/src/main/java/eu/daxiongmao/codility/dynamicProgramming/DynamicProgramming.java
+++ codility/codility/src/main/java/eu/daxiongmao/codility/dynamicProgramming/DynamicProgramming.java
@@ -14,35 +14,105 @@ public class DynamicProgramming {
         if (input.length == 0) { return 0; }
         if (input.length == 1) { return Math.abs(input[0]);}
 
-        final int[] sequence = new int[input.length];
-        // Populate items list
+        System.out.println(String.format("%n*********** Given ***********"));
+        System.out.println(Arrays.toString(input));
+
+        //******************************************************
+        // Step 1. Extract data for later processing
+        //******************************************************
+        System.out.println(" ... Extract data");
+        // items: each entry represents input[i]
         final List<ArrayItem> items = new ArrayList<>();
+        // flag to know if the array is composed of 1 single value
+        boolean alwaysTheSameValue = true;
+        int originalValue = Math.abs(input[0]);
+
         for (int i = 0; i < input.length; i++) {
-            items.add(new ArrayItem(i, input[i]));
+            final int absValue = Math.abs(input[i]);
+            // extract item
+            ArrayItem item = new ArrayItem(input[i], absValue);
+            items.add(item);
+            // Array composition check
+            if (alwaysTheSameValue && originalValue != absValue) {
+                alwaysTheSameValue = false;
+            }
         }
-        // Sort items list
+
+        //******************************************************
+        // Step 2. Basic processing: is the array composed of a single value?
+        //******************************************************
+        if (alwaysTheSameValue) {
+            System.out.println(" ... Input is a repetition of a single number");
+            if (input.length % 2 == 0) {
+                // Even size: min result is always zero. We just have to find the right sequence
+                return 0;
+            } else {
+                // One element remain. It's absolute value is the result
+                return Math.abs(input[0]);
+            }
+        }
+
+        //******************************************************
+        // Step 3. Compute min sum based on items values
+        //******************************************************
+        System.out.println(" ... Processing [natural items]");
+        // Compute permutations
+        int originalSum = computePermutations(items);
+        if (originalSum == 0) { return 0; }
+
+
+        //******************************************************
+        // Step 4. Compute min sum based on grouped values (for instance: sum all '3' or '2' and use global value)
+        //******************************************************
+        System.out.println(" ... Processing [grouped items by vabs]");
+        // Regroup items by absolute values
+        int currentVabs = 0;
+        int currentCount = 0;
+        final List<ArrayItem> groupItems = new ArrayList<>();
+        for (int i = items.size() - 1; i >= 0; i--) {
+            final ArrayItem item = items.get(i);
+            if (currentVabs == item.absoluteValue) {
+                currentCount++;
+            } else {
+                if (currentCount != 0) {
+                    groupItems.add(new ArrayItem(currentVabs * currentCount));
+                }
+                currentVabs = item.absoluteValue;
+                currentCount = 1;
+            }
+        }
+        if (currentCount != 0 && currentVabs != 0) {
+            groupItems.add(new ArrayItem(currentVabs * currentCount));
+        }
+        // Compute permutations
+        int groupedSum = computePermutations(groupItems);
+
+        return (originalSum <= groupedSum ? originalSum : groupedSum);
+    }
+
+    private int computePermutations(final List<ArrayItem> items) {
+        // sort input
         Collections.sort(items);
 
         int sum = 0;
         for (int i = items.size() - 1; i >= 0; i--) {
             final ArrayItem item = items.get(i);
-            System.out.println(item);
-
             if (sum == 0) {
                 // apply current value
                 sum = item.value;
-                sequence[item.index] = item.isNegative() ? -1 : 1;
+                //System.out.println(String.format("Iteration %3d # %s # sum=%5d",i, item, sum));
                 continue;
             }
 
             // Compute sequence and new sum
             int minDifferenceAbs = getMinDifferenceAbs(sum, item.absoluteValue);
-            int sequenceValue = setSequence(sum, item, minDifferenceAbs, sequence);
+            int sequenceValue = setSequence(sum, item, minDifferenceAbs);
             sum = sum + (item.value * sequenceValue);
+            //System.out.println(String.format("Iteration %3d # %s # sum=%5d",i, item, sum));
         }
 
         // print sequence
-        System.out.println("sequence S: " + Arrays.toString(sequence));
+        System.out.println(" ... Sum=" + sum);
         return Math.abs(sum);
     }
 
@@ -50,30 +120,28 @@ public class DynamicProgramming {
         return Math.abs(Math.abs(sum) - itemValueAbs);
     }
 
-    private int setSequence(final int sum, final ArrayItem item, final int minDifferenceAbs, final int[] sequence) {
+    private int setSequence(final int sum, final ArrayItem item, final int minDifferenceAbs) {
         int sequenceValue = 0;
         if (Math.abs(sum + item.value) == minDifferenceAbs) {
             sequenceValue = 1;
         } else {
             sequenceValue = -1;
         }
-        sequence[item.index] = sequenceValue;
         return sequenceValue;
     }
 
     class ArrayItem implements Serializable, Comparable {
-        public final int index;
         public final int value;
         public final int absoluteValue;
 
-        public ArrayItem(final int index, final int value) {
-            this.index = index;
-            this.value = value;
-            this.absoluteValue = Math.abs(value);
+        public ArrayItem(final int absoluteValue) {
+            this.absoluteValue = absoluteValue;
+            this.value = absoluteValue;
         }
 
-        public boolean isNegative() {
-            return this.value < 0;
+        public ArrayItem(final int value, final int absoluteValue) {
+            this.value = value;
+            this.absoluteValue = absoluteValue;
         }
 
         @Override
@@ -89,18 +157,14 @@ public class DynamicProgramming {
             if (this == o) { return true; }
             if (o == null || getClass() != o.getClass()) { return false;}
             ArrayItem arrayItem = (ArrayItem) o;
-            return index == arrayItem.index && value == arrayItem.value && absoluteValue == arrayItem.absoluteValue;
+            return value == arrayItem.value && absoluteValue == arrayItem.absoluteValue;
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(index, value, absoluteValue);
+            return Objects.hash(value, absoluteValue);
         }
 
-        @Override
-        public String toString() {
-            return String.format("item[%s]=%s | abs: %s", index, value, absoluteValue);
-        }
     }
 
 }
--- codility/codility/src/test/java/eu/daxiongmao/codility/dynamicProgramming/DynamicProgrammingTest.java
+++ codility/codility/src/test/java/eu/daxiongmao/codility/dynamicProgramming/DynamicProgrammingTest.java
@@ -8,17 +8,39 @@ public class DynamicProgrammingTest {
     private DynamicProgramming service = new DynamicProgramming();
 
     @Test
+    public void computeSequenceAlt() {
+        Assertions.assertEquals(58, service.computeMinimumSum(new int[] {-30, 100, 10, -2}));
+    }
+
+    @Test
     public void computeSequence() {
+        // exercise example
+        Assertions.assertEquals(0, service.computeMinimumSum(new int[] {1,5,2,-2}));
+
+        // *************************************************************
+        // personal tests cases
+        // *************************************************************
         // Gobal sum is 0. Expect only '1'
         Assertions.assertEquals(0, service.computeMinimumSum(new int[] {1,2,5,2,-10}));
-        // Example: simple case (odd and even are a match)
-        Assertions.assertEquals(0, service.computeMinimumSum(new int[] {1,5,2,-2}));
-        // Advance cases
+        // Advanced cases
         Assertions.assertEquals(58, service.computeMinimumSum(new int[] {-30, 100, 10, -2}));
         Assertions.assertEquals(160, service.computeMinimumSum(new int[] {-1000, 600, 200, 30, 10}));
         Assertions.assertEquals(70, service.computeMinimumSum(new int[] {-1000, 150, 950, 30}));
-        // Codility example [revealed at 2nd failure, 63% overrall]
+        Assertions.assertEquals(0, service.computeMinimumSum(new int[] {1, -1, 1, 1, 1, -5}));
+        // Uniformity
+        Assertions.assertEquals(0, service.computeMinimumSum(new int[] {1, -1, 1, 1, 1, -1}));
+        Assertions.assertEquals(1, service.computeMinimumSum(new int[] {1, -1, 1, 1, 1}));
+        // corner cases
+        Assertions.assertEquals(1, service.computeMinimumSum(new int[] {3, -3, 3, 4, 4}));
+        Assertions.assertEquals(8, service.computeMinimumSum(new int[] {10, -10, 10, 2}));
+        Assertions.assertEquals(0, service.computeMinimumSum(new int[] {1, -1, 1, 1, 2}));
+
+        // *************************************************************
+        // Codility example [revealed at 2nd failure, 63% overall]
+        // *************************************************************
         Assertions.assertEquals(0, service.computeMinimumSum(new int[] {3, 3, 3, 4, 5}));
+        // Another custom example, like Codility
+        Assertions.assertEquals(0, service.computeMinimumSum(new int[] {3, -3, 3, 4, 5}));
     }
 
 }
