a1cd61af56856aa0e27ac4c6f69f2ad98338d5b0
Guillaume Diaz <guillaume@qin-diaz.com>
2022-01-20 20:33:43 +0100

    docker run with bookmark

--- docker/exercices/section_6/src/App.js
+++ docker/exercices/section_6/src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.<br>
-          This is a small change</br>
+          Hi there!  This is a change #2!
         </p>
         <a
           className="App-link"
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React           
+          Learn React
         </a>
       </header>
     </div>
