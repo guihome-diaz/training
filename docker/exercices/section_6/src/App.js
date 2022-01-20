9358ca8575ccdede6c89f7939c15e6b14b1c1279
Guillaume Diaz <guillaume@qin-diaz.com>
2022-01-20 21:25:57 +0100

    docker compose + dockerfile

--- docker/exercices/section_6/src/App.js
+++ docker/exercices/section_6/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!  This is a change #2!
+          Guillaume test
         </p>
         <a
           className="App-link"
