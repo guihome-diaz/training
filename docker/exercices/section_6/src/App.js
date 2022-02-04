03a6b6f0b275051835beb69b9f4d847bf692c3b6
Guillaume Diaz <guillaume@qin-diaz.com>
2022-02-04 22:38:46 +0100

    Completion of the chapter 6

--- docker/exercices/section_6/src/App.js
+++ docker/exercices/section_6/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Guillaume test
+          Edit <code>src/App.js</code> and save to reload. #Guillaume
         </p>
         <a
           className="App-link"
